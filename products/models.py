fd031a683411d4ead28f9cf41d1a55693e855df3
Amir Hossein Ghasemi <amirhossing983@gmail.com>
2022-09-12 01:04:03 -0700

    front

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -11,5 +12,37 @@ class Product(models.Model):
     date_time_created = models.DateTimeField(auto_now_add=True)
     date_time_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
+
+class ActiveCommentManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentManager, self).get_queryset().filter(active=True)
+
+
+class Comment(models.Model):
+    Product_STARS = (
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect')
+    )
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField(verbose_name='comment text')
+    stars = models.CharField(max_length=10, choices=Product_STARS, verbose_name='comment score')
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    date_time_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    # Manager
+    objects = models.Manager()  # default manager
+    active_comment_manager = ActiveCommentManager()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail')
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('commment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create')
 ]
